d0d4f2e211fa1f5ed006b776a959e75ab19d1635
cody <adeyemis710@gmail.com>
2026-08-22 13:40:34 +0100

    setup basic structure of the app, added form, input and label elements with accessibility features

--- apps/src/lib/shoppinglist-app/index.jsx
+++ apps/src/lib/shoppinglist-app/index.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
+import './styles.css'
 
 export const ShoppingList = () => {
   return (
-    <div>
-        
+    <div className="container">
+    <h1>Shopping List</h1>
+      <form>
+        <label htmlFor="search">Search for an item:</label>
+        <input
+          id="search"
+          type="search"
+          placeholder="Search..."
+          aria-describedby="search-description"
+        /> 
+        <p id="search-description">Type to filter the list below:</p>
+      </form>
+
     </div>
   )
 };
