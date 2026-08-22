cf0d7dc07e308f804577f73f01124577f435494e
cody <adeyemis710@gmail.com>
2026-08-22 13:35:26 +0100

    create shopping list app lib to learn memorization strategies in react and create css codes for it

--- apps/src/App.jsx
+++ apps/src/App.jsx
@@ -4,13 +4,13 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 // import { SuperheroForm } from './lib/superhero-application-form'
-import {EventRSVPForm} from './lib/event-rsvp-form'
+// import {EventRSVPForm} from './lib/event-rsvp-form'
 
 function App() {
 
   return (
     // <SuperheroForm />
-    <EventRSVPForm />
+    // <EventRSVPForm />
   )
 }
 
