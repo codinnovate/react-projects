12bfee6849c970c4ea73c64135572fa769829aa8
cody <adeyemis710@gmail.com>
2026-08-27 22:43:56 +0100

    added routing using react-router, created two routes, for superhero-app and eventRSVP form app

--- apps/src/App.jsx
+++ apps/src/App.jsx
@@ -3,14 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-// import { SuperheroForm } from './lib/superhero-application-form'
-// import {EventRSVPForm} from './lib/event-rsvp-form'
+import { SuperheroForm } from './lib/superhero-application-form'
+import {EventRSVPForm} from './lib/event-rsvp-form'
+import {BrowserRouter, Routes, Route} from 'react-router';
 
 function App() {
 
   return (
-    // <SuperheroForm />
-    // <EventRSVPForm />
+    <BrowserRouter>
+    <Routes>
+      <Route 
+      path='/superhero-app' 
+      element={<SuperheroForm />}
+      /> 
+      <Route 
+      path='/event-app'
+      element={<EventRSVPForm />}
+      />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
